3d2eb88400f9a6a15cdcd9bea8c805040565fcf2
HuNanZai <123165587@qq.com>
2016-02-01 18:21:37 +0800

    接入获取帮助的计时类

---
+++ src/Help/Timer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/2/1
+ * Time: 17:20
+ */
+namespace HuNanZai\Component\Log\Help;
+
+class Timer extends \PHP_Timer
+{
+
+}
